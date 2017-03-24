76af715d8a9c26d0a850f70043a3f55f035478a1
Sahra Vadrot <sahra.vadrot@supinternet.fr>
2017-03-24 15:32:58 +0100

    ping

--- pingNetwork.py
+++ pingNetwork.py
@@ -2,9 +2,9 @@ import os
 
 def check_ping():
 
-    hostname = "8.8.8.8"
-    response = os.system("ping -n 1 " + hostname)
-    # and then check the response...
+    hostname = "google.com"
+    response = os.system("ping -t 1" + hostname)
+
     if response == 0:
         ping_status = hostname + " is up"
     else:
