f7dd9073887828be7dbd1d35f54048d3c6512f61
Nathael Arki <nathael.arki@supinternet.fr>
2017-03-24 18:07:36 +0100

    inter

--- interface_graphique.py
+++ interface_graphique.py
@@ -8,27 +8,27 @@ white_line1 = canvas.create_line(0, 150, 200, 150, fill="white")
 white_line1_2 = canvas.create_line(200, 150, 200, 170, fill="white")
 Text1 = canvas.create_text(165, 140, text="SOMETHING")
 
-white_line2 = canvas.create_line(1000, 50, 1200, 50, fill="white")
-white_line2_2 = canvas.create_line(1000, 50, 1000, 70, fill="white")
-Text2 = canvas.create_text(1035, 40, text="SOMETHING")
+white_line2 = canvas.create_line(1000, 150, 1200, 150, fill="white")
+white_line2_2 = canvas.create_line(1000, 150, 1000, 170, fill="white")
+Text2 = canvas.create_text(1035, 140, text="SOMETHING")
 
-white_line3 = canvas.create_line(950, 200, 1200, 200, fill="white")
-white_line3_2 = canvas.create_line(950, 200, 950, 220, fill="white")
-Text3 = canvas.create_text(985, 190, text="SOMETHING")
+white_line3 = canvas.create_line(950, 300, 1200, 300, fill="white")
+white_line3_2 = canvas.create_line(950, 300, 950, 320, fill="white")
+Text3 = canvas.create_text(985, 290, text="SOMETHING")
 
-white_line4 = canvas.create_line(900, 400, 1200, 400, fill="white")
-white_line4_2 = canvas.create_line(900, 400, 900, 420, fill="white")
-Text4 = canvas.create_text(935, 390, text="SOMETHING")
+white_line4 = canvas.create_line(900, 450, 1200, 450, fill="white")
+white_line4_2 = canvas.create_line(900, 450, 900, 470, fill="white")
+Text4 = canvas.create_text(935, 440, text="SOMETHING")
 
 white_line5 = canvas.create_line(0, 300, 250, 300, fill="white")
 white_line5_2 = canvas.create_line(250, 300, 250, 320, fill="white")
-Text5 = canvas.create_text(165, 140, text="SOMETHING")
+Text5 = canvas.create_text(215, 290, text="SOMETHING")
 
 white_line6 = canvas.create_line(0, 450, 300, 450, fill="white")
 white_line6_2 = canvas.create_line(300, 450, 300, 470, fill="white")
-Text6 = canvas.create_text(165, 140, text="SOMETHING")
+Text6 = canvas.create_text(265, 440, text="SOMETHING")
 
-#photo = PhotoImage(file="embleme.PNG") # need resize
+#photo = PhotoImage(file="url") # need resize
 #canvas.create_image(200, 0, anchor=NW, image=photo)
 
 canvas.pack()
