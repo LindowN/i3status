ac522373322859f19ecd4cf29cc8dc4a82e4c7ac
remy peyre <remy.peyre05@gmail.com>
2017-03-24 17:02:52 +0100

    CPU usage DONE

--- loadAverage.py
+++ loadAverage.py
@@ -1,3 +1,10 @@
 import os
 
 print(os.getloadavg())
+
+toto = os.times().user
+#(1.296875, 0.765625, 0.0, 0.0, 0.0)
+print(toto)
+#times() -> (utime, stime, cutime, cstime, elapsed_time)
+
+#Return a tuple of floating point numbers indicating process times.
