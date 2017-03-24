ecc9d4bb2d7e7c8640c32c5c992a8c68d381988f
Sahra Vadrot <sahra.vadrot@supinternet.fr>
2017-03-24 22:46:45 +0100

    temperature

--- temperature.py
+++ temperature.py
@@ -1,7 +1,4 @@
 import wmi
-w = wmi.WMI(namespace="root\OpenHardwareMonitor")
-temperature_infos = w.Sensor()
-for sensor in temperature_infos:
-    if sensor.SensorType==u'Temperature':
-        print(sensor.Name)
-        print(sensor.Value)
+w = wmi.WMI(namespace="root\wmi")
+temperature_info = w.MSAcpi_ThermalZoneTemperature()[0]
+print((temperature_info.CurrentTemperature-2732)/10.0, "celsius")
