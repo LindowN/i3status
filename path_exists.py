f6a68388197cac9ae9c55be397d09a9a4bb7a0ff
remy peyre <remy.peyre05@gmail.com>
2017-03-29 10:57:47 +0200

    modif path exists

--- path_exists.py
+++ path_exists.py
@@ -1,20 +1,31 @@
+#from os import path
+
+#strPath = r"/home/Document/Python/i3status"
+
+#print("the current directoryis: %s" % strPath)
+#print("abspath = %s" % path.abspath(strPath))
+#print("dirname = %s" % path.dirname(strPath))
+#print("basename = %s" % path.basename(strPath))
+#print("exists = %s" % path.exists(strPath))
+#print("isdir = %s" % path.isdir(strPath))
+#print("isfile = %s" % path.isfile(strPath))
+
+
 #import os.path
 
-#if  os.path.exists(file_name)==True: print("I get the file >hubaa")
 
-#if os.path.exists(file_name)==False:
-#      print("The File s% it's not created "%file_name)
-#      os.touch(file_name)
-#      print("The file s% has been Created ..."%file_name)
+#def pathExists(strPath):
+#    if os.path.exists(strPath):
+#        print ("True")
+#        print("exists = %s" % path.exists(strPath))
+#        print("isdir = %s" % path.isdir(strPath))
+#        print("isfile = %s" % path.isfile(strPath))
 
-from os import path
 
-strPath = r"/home/Document/Python/i3status"
+import os.path
 
-print("the current directoryis: %s" % strPath)
-print("abspath = %s" % path.abspath(strPath))
-print("dirname = %s" % path.dirname(strPath))
-print("basename = %s" % path.basename(strPath))
-print("exists = %s" % path.exists(strPath))
-print("isdir = %s" % path.isdir(strPath))
-print("isfile = %s" % path.isfile(strPath))
+strPath = r"/home"
+
+if os.path.exists(strPath):
+    print ("True")
+
