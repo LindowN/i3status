81a38b910baffa25b42ab8cf00c07514ceda4e52
Sahra Vadrot <sahra.vadrot@supinternet.fr>
2017-03-31 15:26:53 +0200

    disk

--- disk.py
+++ disk.py
@@ -1,7 +1,5 @@
 import psutil
 import math
 
-free_space = psutil.disk_usage('/').free / 8 * (10 ** -9)
-print('Free space on disk:', math.floor(free_space), 'Go')
-
-print(psutil.net_if_addrs().broadcast)
+mybytes = psutil.disk_usage('/').free / 1000000000
+print('Free space on disk:', math.floor(mybytes), 'Go')
