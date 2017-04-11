c4d78477e4328e9aad79d3f4de59b831c73df5f9
Sahra Vadrot <sahra.vadrot@supinternet.fr>
2017-04-11 22:07:07 +0200

    push design and test readme

--- README.rst
+++ README.rst
@@ -1,21 +1,21 @@
-###i3status
-
-- [ ] SSID connecté, protocole, BSSID de la borne 
+##i3status
+.. image:: python.png
 - [ ] IP, Ping avg, % de loss de la route par défaut 
 - [ ] Interface Tkinter (S'inspirer de Conky) 
-- [x] Run-watch (Vérifier si un client DHCP est lancé, un watcher gulp, in seveur web de dev, ...). 
-- [x] IP, Ping avg, % de loss du NS 
-- [x] --heure--(fait)
-- [x] --battery--(fait)
-- [x] --Ping de 8.8.8.8 pour vérifier la connection internet--(fait)
-- [x] --IP des interfaces réseau -(fait)
-- [x] --Espace libre sur les partitions --(fait)
-- [x] --Load average / CPU Usage --(fait)
-- [x] --Volume audio --(fait)
-- [x] --Température du CPU --(fait)
-- [x] Nommer les bornes Wi-Fi dans un fichier de conf --(fait)
-- [x] Path Exists (Vérifier si un VPN est ouvert). --(fait)
-- [x] Nommer les bornes Wi-Fi dans un fichier de conf --(fait)
+- [x] Run-watch (Vérifier si un client DHCP est lancé, un watcher gulp, in seveur web de dev, ...). //windows
+- [x] IP, Ping avg, % de loss du NS hybride
+- [x] --heure--(fait) hybride
+- [x] --battery--(fait) hybride
+- [x] --Ping de 8.8.8.8 pour vérifier la connection internet--(fait) hybride
+- [x] --IP des interfaces réseau -(fait) hybride
+- [x] --Espace libre sur les partitions --(fait) hybride
+- [x] --Load average / CPU Usage --(fait) hybride
+- [x] --Volume audio --(fait) windows
+- [x] --Température du CPU --(fait) windows
+- [x] SSID connecté, protocole, BSSID de la borne //mac
+- [x] Nommer les bornes Wi-Fi dans un fichier de conf --(fait) windows
+- [x] Path Exists (Vérifier si un VPN est ouvert). --(fait) windows
+- [x] Nommer les bornes Wi-Fi dans un fichier de conf --(fait) windows
 
 
 - [ ] BuildBot
--- interface_graphique.py
+++ interface_graphique.py
@@ -116,15 +116,15 @@ def maj():
     canvas.delete(Percent1)
     canvas.delete(Percent2)
     canvas.delete(Text5_2)
-    Text2 = canvas.create_text(1055, 165, text=giveMeTime(), font=orbiclean)
-    Text3 = canvas.create_text(1055, 315, text=giveMeBattery(), font=orbiclean)
+    Text2 = canvas.create_text(1055, 165, text=giveMeTime(), font=orbiclean,fill = font_white)
+    Text3 = canvas.create_text(1055, 315, text=giveMeBattery(), font=orbiclean, fill = font_white)
     random1 = 1035 + giveMePercentBattery()*1.6
     Rect2 = canvas.create_rectangle(1035, 10, random1, 35, fill="#00FF00") 
     Percent1 = canvas.create_text(1110, 20, text=str(giveMePercentBattery())+'%', font=orbiclean, fill='white')
     random2 = 590 - (giveMeVolume()) * 1.6
     Rect4 = canvas.create_rectangle(1155, random2, 1190, 590, fill="red")
     Percent2 = canvas.create_text(1173, 510, text=str(giveMeVolume()), font=orbiclean, fill='white')
-    Text5_2 = canvas.create_text(170, 315, text=giveMeIpPing(), font=orbiclean)
+    Text5_2 = canvas.create_text(170, 315, text=giveMeIpPing(), font=orbiclean, fill = font_white)
     fenetre.after(1000,maj)
 
 def wampMaj():
@@ -146,24 +146,26 @@ font.families()
 fenetre.resizable(width=False, height=False)
 canvas = Canvas(fenetre, width=1200, height=600, background="#DBDBDB", cursor="arrow")
 canvas.pack()
-
+photo = PhotoImage(file="xana.png") # need resize
+canvas.create_image(0, 0, anchor=NW, image=photo)
 colorLines = "white"
 colorLetters = "black"
+font_white = "white"
 
 white_line1 = canvas.create_line(0, 150, 200, 150, fill=colorLines)
 white_line1_2 = canvas.create_line(200, 150, 200, 170, fill=colorLines)
-Text1 = canvas.create_text(140, 140, text="Temperature", font=FatBigHyppo)
-Text1_2 = canvas.create_text(155, 165, text=giveMeTemperature(), font=orbiclean)
+Text1 = canvas.create_text(140, 140, text="Temperature", font=FatBigHyppo, fill = font_white )
+Text1_2 = canvas.create_text(155, 165, text=giveMeTemperature(), font=orbiclean,fill = font_white)
 
 white_line2 = canvas.create_line(950, 150, 1200, 150, fill=colorLines)
 white_line2_2 = canvas.create_line(950, 150, 950, 170, fill=colorLines)
-Text2_title = canvas.create_text(1020, 140, text='Date and Time', font=FatBigHyppo)
-Text2 = canvas.create_text(985, 165, text=giveMeTime(), font=orbiclean)
+Text2_title = canvas.create_text(1020, 140, text='Date and Time', font=FatBigHyppo, fill = font_white)
+Text2 = canvas.create_text(985, 165, text=giveMeTime(), font=orbiclean, fill = font_white)
 
 white_line3 = canvas.create_line(950, 300, 1200, 300, fill=colorLines)
 white_line3_2 = canvas.create_line(950, 300, 950, 320, fill=colorLines)
-Text3_title = canvas.create_text(985, 290, text='Battery', font=FatBigHyppo)
-Text3 = canvas.create_text(985, 315, text=giveMeBattery(), font=orbiclean)
+Text3_title = canvas.create_text(985, 290, text='Battery', font=FatBigHyppo, fill = font_white)
+Text3 = canvas.create_text(985, 315, text=giveMeBattery(), font=orbiclean, fill = font_white)
 Rect1 = canvas.create_rectangle(1035, 10, 1195, 35, fill=colorLetters)
 random1 = 1035 + giveMePercentBattery()*1.6
 Rect2 = canvas.create_rectangle(1035, 10, random1, 35, fill="#FD3F92")
@@ -171,8 +173,8 @@ Percent1 = canvas.create_text(1100, 20, text=str(giveMePercentBattery())+'%', fo
 
 white_line4 = canvas.create_line(950, 400, 1200, 400, fill=colorLines)
 white_line4_2 = canvas.create_line(950, 400, 950, 420, fill=colorLines)
-Text4_Title = canvas.create_text(975, 390, text="Disk", font=FatBigHyppo)
-Text4 = canvas.create_text(1040, 415, text=giveMeRemainingSpace(), font=orbiclean)
+Text4_Title = canvas.create_text(975, 390, text="Disk", font=FatBigHyppo, fill = font_white)
+Text4 = canvas.create_text(1040, 415, text=giveMeRemainingSpace(), font=orbiclean,fill = font_white)
 Rect3 = canvas.create_rectangle(1155, 430, 1190, 590, fill=colorLetters)
 random2 = 590 - (giveMeVolume()) * 1.6
 Rect4 = canvas.create_rectangle(1155, random2, 1190, 590, fill="red")
@@ -181,21 +183,19 @@ Percent2 = canvas.create_text(1173, 510, text=str(giveMeVolume()), font=orbiclea
 white_line5 = canvas.create_line(0, 300, 250, 300, fill=colorLines)
 white_line5_2 = canvas.create_line(250, 300, 250, 320, fill=colorLines)
 Text5 = canvas.create_text(215, 290, text="PING", font=FatBigHyppo)
-Text5_2 = canvas.create_text(170, 315, text=giveMeIpPing(), font=orbiclean)
-Text5_3 = canvas.create_text(170, 335, text=giveMeIpV4(), font=orbiclean)
+Text5_2 = canvas.create_text(170, 315, text=giveMeIpPing(), font=orbiclean,fill = font_white)
+Text5_3 = canvas.create_text(170, 335, text=giveMeIpV4(), font=orbiclean, fill = font_white)
 
 white_line6 = canvas.create_line(0, 450, 300, 450, fill=colorLines)
 white_line6_2 = canvas.create_line(300, 450, 300, 470, fill=colorLines)
-Text6_Title = canvas.create_text(265, 440, text='Wifi', font=FatBigHyppo)
-Text6 = canvas.create_text(200, 460, text=giveMeWifi(), font=orbiclean)
+Text6_Title = canvas.create_text(265, 440, text='Wifi', font=FatBigHyppo, fill = font_white)
+Text6 = canvas.create_text(200, 460, text=giveMeWifi(), font=orbiclean, fill = font_white)
 
-Text7 = canvas.create_text(40,  15, text="DHCP : ", font=FatBigHyppo)
+Text7 = canvas.create_text(40,  15, text="DHCP : ", font=FatBigHyppo, fill = font_white)
 Text7_2 = canvas.create_text(100, 15, text="Loading...", fill='red', font=orbiclean)
-Text8 = canvas.create_text(40, 35, text="WAMP : ", font=FatBigHyppo)
+Text8 = canvas.create_text(40, 35, text="WAMP : ", font=FatBigHyppo, fill = font_white)
 Text8_2 = canvas.create_text(100, 35, text="Loading...", fill='red',  font=orbiclean)
 
-photo = PhotoImage(file="bg.png") # need resize
-canvas.create_image(450, 175, anchor=NW, image=photo)
 
 
 maj()
